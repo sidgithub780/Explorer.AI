1e3126e5e8e06d406c67e0b954993ee827339572
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2023-09-16 12:16:08 -0400

    unstyled login page

--- frontend-travelio/src/components/Navbar.js
+++ frontend-travelio/src/components/Navbar.js
@@ -1,26 +1,20 @@
 // Navbar.js
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-green-300 text-black p-4 z-50">
       <ul className="flex justify-end space-x-4 pr-5">
         <li>
-          <a href="/" className="text-black text-lg">
-            Home
-          </a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/about" className="text-black text-lg">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/contact" className="text-black text-lg">
-            Contact
-          </a>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
+
+      <Outlet />
     </nav>
   );
 };
