902283f85716a871cfa3b6998a38b73b85bca1ba
vingupta22 <vinay.gupta.489@k12.friscoisd.org>
2023-09-16 16:56:00 -0400

    This is a fully functioning version with 2/3 MVP complete

--- backend-travelio/main.py
+++ backend-travelio/main.py
@@ -2,6 +2,8 @@ import os
 import openai
 import subprocess
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
 
 #supabase stuff
 from supabase import create_client #import supabase
@@ -20,33 +22,57 @@ profile_age = ""
 
 #flask implementation
 app = Flask(__name__)
-@app.route('/', methods=['POST']) #replace endpoint later
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/', methods=['POST'])
+@cross_origin() 
 def generate_itinerary():
     #data = request.form.get_json()
 
     # Extract data from the JSON payload sent by the front end
-    destination = request.form.get('destination')
-    duration = request.form.get('duration')
-    month = request.form.get('month')
-    style = request.form.get('style')
-    people = request.form.get('people')
-    budget = request.form.get('budget')
+    try:
+        data = request.get_json()  # Use request.get_json() to parse JSON data
+        destination = data.get('destination')
+        duration = data.get('duration')
+        month = data.get('month')
+        style = data.get('style')
+        people = data.get('people')
+        budget = data.get('budget')
+        if destination is None or duration is None or month is None or style is None or people is None or budget is None:
+            return jsonify({"error": "One or more required fields are missing in the form data."}), 400
+        print("This is destination: ", destination)
+        print(type(destination))
+        print("This is duration: ", duration)
+        print(type(duration))
+        print("This is month: ", month)
+        print(type(month))
+        print("This is style: ", style)
+        print(type(style))
+        print("This is people: ", people)
+        print(type(people))
+        print("This is budget: ", budget)
+        print(type(budget))
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
     #General questions 
+    """
     destination = input("Please enter the destination you are planning to visit: ") #These may be uneccesary, because of form data. THis is old.
     duration = input("Please enter the number of days you plan to stay: ")
     month = input("Please enter the month you plan to travel: ")
     style = input("Please enter which vacation style you prefer (relaxed, moderate, fast-paced): ")
     people = input("How many people will be traveling? (Enter 1 if alone): ")
     budget = input("What is your budget? (low/medium/high)")
+    """
 
     #If profile is NOT configured
     if(profile_likes == "" and profile_dislikes == "" and profile_experience == "" and profile_age == ""):
         #transport here (rental car, etc)
         gptString = (
             "\"Please create a detailed, day by day vacation plan to " 
-            + destination + " with a duration of " + duration + " days with a " 
-            + style + " pace during the month of " + month + " built around a trip with " + people + " people" + "with a  " + budget + " budget" +
+            + destination + " with a duration of " + str(duration) + " days with a " 
+            + style + " pace during the month of " + month + " built around a trip with " + str(people) + " people" + "with a  " + budget + " budget" +
             ". Make sure to add a *** between each day so it is easy to separate the days, and label activities by with what time of day it would occur" + "\""
         )
         cmd = "sgpt txt " + gptString
